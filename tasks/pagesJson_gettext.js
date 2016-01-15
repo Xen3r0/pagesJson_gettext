1144879e331912a6222b65aba647e40654f9168b
xen3r0 <m.santisteban@gammasoftware.fr>
2016-01-15 10:31:18 +0100

    Init depot

---
+++ tasks/pagesJson_gettext.js
@@ -0,0 +1,45 @@
+/*
+ * grunt-pagesJson_gettext
+ * https://github.com/Xen3r0/pagesJson_gettext
+ *
+ * Copyright (c) 2016 xen3r0
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+    grunt.registerMultiTask('pagesJson_gettext', 'Conversion d\'un fichier JSON de routage en fichier JS.', function () {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            punctuation: '.',
+            separator: ', '
+        });
+
+        // Iterate over all specified file groups.
+        this.files.forEach(function (f) {
+            // Concat specified files.
+            var src = f.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function (filepath) {
+                // Read file source.
+                return grunt.file.read(filepath);
+            }).join(grunt.util.normalizelf(options.separator));
+
+            // Handle options.
+            src += options.punctuation;
+
+            // Write the destination file.
+            grunt.file.write(f.dest, src);
+
+            // Print a success message.
+            grunt.log.writeln('File "' + f.dest + '" created.');
+        });
+    });
+};
